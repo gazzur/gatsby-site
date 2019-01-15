200a8365321145447cfd16232e2e37d409508992
Gary Ebsworth <gary@cosmic.org.uk>
2019-01-15 14:51:24 +0000

    restyle post template

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,33 +1,26 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from '../components/layout'
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark
+
+export default ({ data }) => {
+  const post = data.markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+     <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
-    </div>
+    </Layout>   
   )
 }
 
-export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
         title
-        path
       }
     }
   }
