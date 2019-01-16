e399082b3f2d5b688cc80622bf0a702c7e764bb2
Gazzur <gazzur@hotmail.com>
2019-01-16 19:53:09 +0000

    alter markdown pages location

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/`,
+        path: `${__dirname}/src/posts`,
         name: "markdown-pages",
       },
     },
