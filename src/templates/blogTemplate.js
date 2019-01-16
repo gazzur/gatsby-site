686112b6053ed51128c6bf30103c4a6ef040d003
Gazzur <gazzur@hotmail.com>
2019-01-16 17:10:50 +0000

    add image processing for featured images

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,14 +1,14 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from '../components/layout'
-import Postpic from '../components/postpic'
-
+import PostImage from 'gatsby-image'
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
      <Layout>
       <div>
+      <PostImage fixed={post.frontmatter.thumbnail.childImageSharp.fixed} alt="A Cool Dinner" title={post.frontmatter.title} />
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
@@ -22,7 +22,13 @@ export const query = graphql`
       html
       frontmatter {
         title,
-        thumbnail
+        thumbnail{
+          childImageSharp {
+            fixed(width: 980) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
       }
     }
   }
